2b8947207f784d57a529da761c033832b6192714
praveen kumar <praveen.kumar@microchip.com>
2021-07-29 21:43:07 +0530

    v4l2_subdev_entity code added
    Signed-off-by: praveen kumar <praveen.kumar@microchip.com>

--- files/v4l2-microchip.c
+++ files/v4l2-microchip.c
@@ -39,6 +39,8 @@
 #include <media/videobuf2-v4l2.h>
 #include <media/videobuf2-dma-contig.h>
 #include <linux/platform_device.h>
+#include <media/v4l2-fwnode.h>
+#include <media/v4l2-subdev.h>
 
 MODULE_DESCRIPTION("V4L2 Skeleton Driver");
 MODULE_AUTHOR("Microchip");
@@ -78,12 +80,81 @@ struct skeleton {
 	struct list_head buf_list;
 	unsigned field;
 	unsigned sequence;
+
+	struct v4l2_subdev_entity	*current_subdev;
+	struct list_head		subdev_entities;
 };
 
 struct skel_buffer {
 	struct vb2_v4l2_buffer vb;
 	struct list_head list;
 };
+
+struct v4l2_subdev_entity {
+	struct v4l2_subdev		*sd;
+	struct v4l2_async_subdev	*asd;
+	struct device_node		*epn;
+	struct v4l2_async_notifier      notifier;
+
+	u32 pfe_cfg0;
+
+	struct list_head list;
+};
+
+static int v4l2_parse_dt(struct device *dev, struct skeleton *skel)
+{
+	struct device_node *np = dev->of_node;
+	struct device_node *epn = NULL;
+	struct v4l2_subdev_entity *subdev_entity;
+	unsigned int flags;
+	int ret;
+
+	INIT_LIST_HEAD(&skel->subdev_entities);
+
+	while (1) {
+		struct v4l2_fwnode_endpoint v4l2_epn = { .bus_type = 0 };
+
+		epn = of_graph_get_next_endpoint(np, epn);
+		if (!epn)
+			return 0;
+
+		ret = v4l2_fwnode_endpoint_parse(of_fwnode_handle(epn),
+						 &v4l2_epn);
+		if (ret) {
+			ret = -EINVAL;
+			dev_err(dev, "Could not parse the endpoint\n");
+			break;
+		}
+
+		subdev_entity = devm_kzalloc(dev, sizeof(*subdev_entity),
+					     GFP_KERNEL);
+		if (!subdev_entity) {
+			ret = -ENOMEM;
+			break;
+		}
+		subdev_entity->epn = epn;
+
+		flags = v4l2_epn.bus.parallel.flags;
+
+		if (flags & V4L2_MBUS_HSYNC_ACTIVE_LOW)
+			subdev_entity->pfe_cfg0 = ISC_PFE_CFG0_HPOL_LOW;
+
+		if (flags & V4L2_MBUS_VSYNC_ACTIVE_LOW)
+			subdev_entity->pfe_cfg0 |= ISC_PFE_CFG0_VPOL_LOW;
+
+		if (flags & V4L2_MBUS_PCLK_SAMPLE_FALLING)
+			subdev_entity->pfe_cfg0 |= ISC_PFE_CFG0_PPOL_LOW;
+
+		if (v4l2_epn.bus_type == V4L2_MBUS_BT656)
+			subdev_entity->pfe_cfg0 |= ISC_PFE_CFG0_CCIR_CRC |
+					ISC_PFE_CFG0_CCIR656;
+
+		list_add_tail(&subdev_entity->list, &skel->subdev_entities);
+	}
+	of_node_put(epn);
+
+	return ret;
+}
 
 static inline struct skel_buffer *to_skel_buffer(struct vb2_v4l2_buffer *vbuf)
 {
@@ -744,6 +815,72 @@ static const struct v4l2_file_operations skel_fops = {
 	.poll = vb2_fop_poll,
 };
 
+static int v4l2_async_bound(struct v4l2_async_notifier *notifier,
+			    struct v4l2_subdev *subdev,
+			    struct v4l2_async_subdev *asd)
+{
+	struct skeleton *skel = container_of(notifier->v4l2_dev,
+					     struct skeleton *skel, v4l2_dev);
+	struct v4l2_subdev_entity *subdev_entity =
+		container_of(notifier, struct v4l2_subdev_entity, notifier);
+
+	if (video_is_registered(&skel->vdev)) {
+		v4l2_err(&skel->vdev, "only supports one sub-device.\n");
+		return -EBUSY;
+	}
+
+	subdev_entity->sd = subdev;
+
+	return 0;
+}
+
+static void v4l2_async_unbind(struct v4l2_async_notifier *notifier,
+			      struct v4l2_subdev *subdev,
+			      struct v4l2_async_subdev *asd)
+{
+	struct skeleton *skel = container_of(notifier->v4l2_dev,
+					     struct skeleton *skel, v4l2_dev);
+//	cancel_work_sync(&skel->awb_work);
+	video_unregister_device(&skel->vdev);
+	v4l2_ctrl_handler_free(&skel->ctrl_handler);
+}
+
+static int v4l2_async_complete(struct v4l2_async_notifier *notifier)
+{
+	int ret = 0;
+	struct skeleton *skel = container_of(notifier->v4l2_dev,
+					     struct skeleton *skel, v4l2_dev);
+
+	ret = v4l2_device_register_subdev_nodes(&skel->v4l2_dev);
+	if (ret < 0) {
+		v4l2_err(&skel->v4l2_dev, "Failed to register subdev nodes\n");
+		return ret;
+	}
+
+	skel->current_subdev = container_of(notifier,
+					   struct v4l2_subdev_entity, notifier);
+
+	return 0;
+}
+
+const struct v4l2_async_notifier_operations v4l2_async_ops = {
+	.bound = v4l2_async_bound,
+	.unbind = v4l2_async_unbind,
+	.complete = v4l2_async_complete,
+};
+
+void v4l2_subdev_cleanup(struct skeleton *skel)
+{
+	struct v4l2_subdev_entity *subdev_entity;
+
+	list_for_each_entry(subdev_entity, &skel->subdev_entities, list) {
+		v4l2_async_notifier_unregister(&subdev_entity->notifier);
+		v4l2_async_notifier_cleanup(&subdev_entity->notifier);
+	}
+
+	INIT_LIST_HEAD(&skel->subdev_entities);
+}
+
 /*
  * The initial setup of this device instance. Note that the initial state of
  * the driver should be complete. So the initial format, standard, timings
@@ -755,6 +892,7 @@ static int v4l2_microchip_probe(struct platform_device *pdev)
 	static const struct v4l2_dv_timings timings_def = V4L2_DV_BT_CEA_1280X720P60;
 	struct skeleton *skel;
 	struct video_device *vdev;
+	struct v4l2_subdev_entity *subdev_entity;
 	struct v4l2_ctrl_handler *hdl;
 	struct vb2_queue *q;
 	int ret;
@@ -786,6 +924,49 @@ static int v4l2_microchip_probe(struct platform_device *pdev)
 	ret = v4l2_device_register(&pdev->dev, &skel->v4l2_dev);
 	if (ret)
 		goto disable_v4l2;
+
+	ret = v4l2_parse_dt(&pdev->dev, skel);
+	if (ret) {
+		dev_err(dev, "fail to parse device tree\n");
+		goto free_hdl;
+	}
+
+	if (list_empty(&skel->subdev_entities)) {
+		dev_err(dev, "no subdev found\n");
+		ret = -ENODEV;
+		goto free_hdl;
+	}
+
+	list_for_each_entry(subdev_entity, &skel->subdev_entities, list) {
+		struct v4l2_async_subdev *asd;
+
+		v4l2_async_notifier_init(&subdev_entity->notifier);
+
+		asd = v4l2_async_notifier_add_fwnode_remote_subdev(
+					&subdev_entity->notifier,
+					of_fwnode_handle(subdev_entity->epn),
+					struct v4l2_async_subdev);
+
+		of_node_put(subdev_entity->epn);
+		subdev_entity->epn = NULL;
+
+		if (IS_ERR(asd)) {
+			ret = PTR_ERR(asd);
+			goto cleanup_subdev;
+		}
+
+		subdev_entity->notifier.ops = &v4l2_async_ops;
+
+		ret = v4l2_async_notifier_register(&skel->v4l2_dev,
+						   &subdev_entity->notifier);
+		if (ret) {
+			dev_err(dev, "fail to register async notifier\n");
+			goto cleanup_subdev;
+		}
+
+		if (video_is_registered(&skel->video_dev))
+			break;
+	}
 
 	mutex_init(&skel->lock);
 
@@ -881,6 +1062,8 @@ static int v4l2_microchip_probe(struct platform_device *pdev)
 free_hdl:
 	v4l2_ctrl_handler_free(&skel->ctrl_handler);
 	v4l2_device_unregister(&skel->v4l2_dev);
+cleanup_subdev:
+	v4l2_subdev_cleanup(skel);
 disable_v4l2:
 	return ret;
 }
